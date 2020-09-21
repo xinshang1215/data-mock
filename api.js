21d039a26de647346e0d3c644618443ba96e60c2
xinshang1215 <94335897@qq.com>
2020-09-21 17:08:06 +0800

    配置

---
+++ api.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: M.M
+ * @Date:   2020-09-21 14:35:38
+ * @Last Modified by:   M.M
+ * @Last Modified time: 2020-09-21 16:37:33
+ */
+'use strict';
+
+const 
+
+import MD from './mockDatas'
+
+
+const api = {
+    "enrollTable":
+}
+export default api
--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+/*
+ * @Author: M.M
+ * @Date:   2020-09-21 11:36:50
+ * @Last Modified by:   M.M
+ * @Last Modified time: 2020-09-21 15:44:03
+ */
+'use strict';
+
 //引入koa
 const Koa = require("koa")
-
+const router = require("koa-router")
 //实例化koa对象
 const app = new Koa()
 
-app.use(async(ctx,next)=>{
-    await next();
-    ctx.response.type = "text/html";
-    ctx.response.body = "<h1>Hellp World</h1>"
-})
 
+
+
+
+
+
+
+app.use(router.routes()) //启动路由
+app.use(router.allowedMethods()) //只允许当前设置的方法请求通过
 
 app.listen(8088)
 
