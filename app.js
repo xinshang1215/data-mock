ce54b5896bdea98a11a0c0841ada560d779c4c8f
xinshang1215 <94335897@qq.com>
2020-09-22 16:18:32 +0800

    配置

--- app.js
+++ app.js
@@ -2,21 +2,26 @@
  * @Author: M.M
  * @Date:   2020-09-21 11:36:50
  * @Last Modified by:   M.M
- * @Last Modified time: 2020-09-21 15:44:03
+ * @Last Modified time: 2020-09-22 16:15:10
  */
 'use strict';
 
 //引入koa
-const Koa = require("koa")
-const router = require("koa-router")
+const Koa = require("koa");
+const Router = require("koa-router");
+
 //实例化koa对象
-const app = new Koa()
+const app = new Koa();
+const router = new Router();
 
 
 
 
 
-
+router.get("/", (ctx, next) => {
+    ctx.body = "xxxx"
+    next()
+})
 
 
 app.use(router.routes()) //启动路由
